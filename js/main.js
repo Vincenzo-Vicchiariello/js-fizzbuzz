f8497d10d2ec26cc25b5e206b6c8aed6c24f3259
Vincenzo-Vicchiariello <v.vicchiariello@outlook.it>
2023-03-30 16:49:46 +0200

    Dato ombra, contorno e forma circolare ai riquadri.

--- js/main.js
+++ js/main.js
@@ -9,3 +9,34 @@ for (var i = 1; i < 101; i++) {
 
 }
 
+
+for (var i = 1; i < 101; i++) {
+    if (i == 69) document.getElementById("fb").innerHTML += `
+    <div class="square cs magenta shadow-sm">
+        <p>Nice.</p>
+    </div>
+    `;
+    else if (i % 15 == 0) document.getElementById("fb").innerHTML += `
+    <div class="square cs red shadow-sm">
+        <p>FizzBuzz</p>
+    </div>
+    `;
+    else if (i % 5 == 0) document.getElementById("fb").innerHTML += `
+    <div class="square cs orange shadow-sm">
+        <p>Buzz</p>
+    </div>
+    `;
+    else if (i % 3 == 0) document.getElementById("fb").innerHTML += `
+    <div class="square cs yellow shadow-sm">
+        <p>Fizz</p>
+    </div>
+    `;
+
+    else document.getElementById("fb").innerHTML += `
+    <div class="square cs shadow-sm">
+        <p>${i}</p>
+    </div>
+    `;
+
+
+}
